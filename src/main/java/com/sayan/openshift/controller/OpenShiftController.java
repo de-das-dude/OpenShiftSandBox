f7d37004ca7bbd6c005e374ebbbdc69aae516d13
dedas <dedas@host.docker.internal>
2022-02-17 22:21:21 +0530

    added api

---
+++ src/main/java/com/sayan/openshift/controller/OpenShiftController.java
@@ -0,0 +1,13 @@
+package com.sayan.openshift.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OpenShiftController {
+
+	@GetMapping(path = "/hello")
+	public String hello() {
+		return "hello";
+	}
+}
