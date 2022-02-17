6bf489469b4bca93c02ada5e933c7e3c908fb0f1
dedas <dedas@host.docker.internal>
2022-02-17 21:29:35 +0530

    init

---
+++ src/main/java/com/sayan/openshift/OpenShiftSandBoxApplication.java
@@ -0,0 +1,13 @@
+package com.sayan.openshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenShiftSandBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenShiftSandBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sayan/openshift/OpenShiftSandBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sayan.openshift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenShiftSandBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
